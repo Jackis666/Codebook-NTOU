61412d3ebe315ef38e9b0149868d86bc6fe30be5
Jackis666 . <qqazwwsxeedcrrfvttgb@gmail.com>
2026-08-13 02:03:47 +0800

    更新矩陣乘法

--- math/pow.cpp
+++ math/pow.cpp
@@ -12,16 +12,16 @@ int ksm(int x, int y) {
 // 矩陣乘法
 vector<vector<int>> mul(const vector<vector<int>> &A,
                         const vector<vector<int>> &B) {
-    int n = A.size();
-    vector<vector<int>> C(n, vector<int>(n, 0));
-
-    for (int i = 0; i < n; i++) {
-        for (int k = 0; k < n; k++) if (A[i][k]) {
-            for (int j = 0; j < n; j++) {
-                C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % MOD;
-            }
-        }
-    }
+    if (A.empty() || B.empty() || A[0].empty() || B[0].empty()) { return {};}
+    int m = A.size();
+    int k = A[0].size();
+    int n = B[0].size();
+    vector<vector<int>> C(m, vector<int>(n, 0));
+    for (int i = 0; i < m; i++) {
+    for (int p = 0; p < k; p++) if (A[i][p]) {
+    for (int j = 0; j < n; j++) {
+    C[i][j] = (C[i][j] + 1LL * A[i][p] * B[p][j])%MOD;
+}}}
     return C;
 }
 
