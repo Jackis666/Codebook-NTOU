7b5f953ba3092a1813db301e24702d6fa77560d6
Jackis666 . <qqazwwsxeedcrrfvttgb@gmail.com>
2026-08-14 00:22:01 +0800

    idk

--- math/Pollard_Rho.cpp
+++ math/Pollard_Rho.cpp
@@ -1,13 +1,15 @@
 //回傳n的其中一個因數
-ll f(ll x, ll c, ll mod){ return add(mul(x,x,mod),c,mod); }
-ll pollard_rho(ll n) {
-    ll c = 1, x = 0, y = 0, p = 2, q, t = 0;
-    while (t++ % 128 or __gcd(p, n) == 1) {
+// does not work when n is prime  O(n^(1/4))
+#define LL __int128
+LL f(LL x, LL c, LL mod){ return add(mul(x,x,mod),c,mod); }
+LL pollard_rho(LL n) {
+    LL c = 1, x = 0, y = 0, p = 2, q, t = 0;
+    while (t++ % 128 or gcd(p, n) == 1) {
         if (x == y) c++, y = f(x = 2, c, n);
         if (q = mul(p, abs(x-y), n)) p = q;
         x = f(x, c, n); y = f(f(y, c, n), c, n);
     }
-    return __gcd(p, n);
+    return gcd(p, n);
 }
 /*function<void(int)> fact=[&](int x){
         if(x==1) return;
